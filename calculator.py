b6b5ec2ebed9f91f301b33e53d48676ea9d78513
swedha19 <53312101+swedha19@users.noreply.github.com>
2020-02-24 14:58:52 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+add = float(a)+float(b)
+sub = float(a)-float(b)
+mul = float(a)*float(b)
+div = a/b
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {round(div, 2)}")
+© 2020 GitHub, Inc.
