2d339255fff1972749052e41e302f1fbf3b41818
A Swedha <swedhaalex@gmail.com>
2020-02-26 22:27:38 -1200

    Create hcf.py

---
+++ hcf.py
@@ -0,0 +1,14 @@
+def compute_hcf(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller + 1):
+        if(x%i==0) and(y%i==0):
+           hcf=i
+    return hcf
+
+
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+print("the HCF is",compute_hcf(num1,num2))
