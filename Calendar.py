a4c5fd7d29d802682f6f6d46de545ba5abf83f1f
A Swedha <swedhaalex@gmail.com>
2020-02-26 21:51:52 -1200

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,4 +1,16 @@
+#to print the month of the year
 import calendar
 yy=int(input("enter year:"))
 mm=int(input("enter month:"))
 print(calendar.month(yy,mm))
+
+#to check it is leap year or not
+import calendar
+yy=int(input("enter year:"))
+mm=int(input("enter month:"))
+print(calendar.month(yy,mm))
+
+#to print all the months of year
+import calendar
+yy=int(input("enter year:"))
+print(calendar.calendar(yy))
